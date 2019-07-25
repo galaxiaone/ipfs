a938a4adbce92f4a0c1a3f0586e43af61339b6ce
kyledewy <dewhurst.kyle@protonmail.ch>
2019-07-25 18:43:10 +0200

    now deploys assets straight to ipfs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
+const fs = require('fs');
 const Web3 = require('web3');
 const IPFS = require('ipfs');
 const GALAXIA = require("./contracts/Galaxia.json");
 const TOKEN_URIS = require('./assets/asset-data.json');
-const NUM_PLANETS = TOKEN_URIS.keys.length;
+const IMAGES_DIR = "./assets/images/";
+const METADATA_DIR = "./assets/metadata/";
+const outputFile = "./assets/deployed-assets.json";
+
 // Set network information
 const networkIDs = { 'ropsten': 3, 'rinkeby': 4 };
 const network = 'rinkeby';
@@ -26,173 +30,165 @@ console.log("ADDRESS ", galaxiaInstance.address);
 // const baseURI = await galaxiaInstance.methods.baseTokenURI().call();
 // console.log("Base URI is ", baseURI);
 
-// TODO: Filter for transfers from 0x00000 && pin files when URI is changed for testing
+// ------------------Upload Images------------------------------
+const GifDir = IMAGES_DIR + 'v1';
+const MetaDir = METADATA_DIR + 'v1';
+let gifFiles = [];
+let metadataFiles = [];
+
+// let assetData = JSON.stringify(TOKEN_URIS);
+let assetData = [];
+let numAssets = 0;
+
+const addToIPFS = (filePath) => {
+    const imageBuffer = Buffer.from(filePath);
+
+    ipfs.add(imageBuffer, { pin: true }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        // console.log("planet ", assetName, " hash ", result[0].hash);
+        const hash = result[0].hash;
+    });
+}
+
+// Get planet image files
+fs.readdir(GifDir, function (err, files) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    gifFiles = files;
+    numAssets += files.length;
+    // console.log(files);
+})
+
+// Get planet metadata files 
+fs.readdir(MetaDir, function (err, files) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    metadataFiles = files;
+    numAssets += files.length;
+    // console.log(files);
+})
+
+if (metadataFiles.length !== gifFiles.length) {
+    console.log("There are ", metadataFiles.length, " metadata files ", " and ", gifFiles.length, " image files");
+    process.exit(1);
+}
+
 ipfs.once('ready', () => {
 
     console.log("IPFS node is ready ");
-    for (let i = 0; i < NUM_PLANETS; i++) {
-        const key = TOKEN_URIS.keys[i];
-        const planet = TOKEN_URIS[key];
-        // console.log("name ", key);
-        // console.log(" details ", planet);
-        if (planet.id !== i) {
-            console.log("Planet ID mismatch");
-            return false;
-        }
-        planet.metadata = planet.metadata.slice(5);
-        planet.image = planet.image.slice(5);
-        console.log(key, " metadata: ", planet.metadata);
-        console.log(key, " image ", planet.image);
-
-        ipfs.pin.ls(planet.metadata, (err, pinset) => {
+    // loop through planets
+    for (let i = 0; i < gifFiles.length; i++) {
+        const GIF = gifFiles[i];
+        const Metadata = metadataFiles[i];
+        const assetName = GIF.slice(0, GIF.length - 4);
+        const data = { id: "", imageHash: "", metadata: "" };
+        const gifLocation = GifDir + GIF;
+        const metadataLocation = METADATA_DIR + Metadata;
+        console.log("planet name ", assetName);
+        data.id = i;
+        data.name = assetName;
+        const imageBuffer = Buffer.from(gifLocation);
+        const metadataBuffer = Buffer.from(metadataLocation);
+        ipfs.add(imageBuffer, { pin: true }, (err, result) => {
             if (err) {
-                // console.log(err);
-                ipfs.pin.add(planet.metadata, function (err) {
-                    if (err) {
-                        console.log(err, planet.metadata);
-                    }
-                    console.log("Pinned planet metadata ", planet.metadata);
-                });
+                console.log(err);
+                return false;
             }
-            if (pinset) {
-                console.log('planet metadata already pinned', pinset, planet.metadata);
-            }
-        });
+            console.log("planet ", assetName, " hash ", result[0].hash);
+            data.imageHash = result[0].hash;
 
-        ipfs.pin.ls(planet.image, (err, pinset) => {
-            if (err) {
-                // console.log(err);
-                ipfs.pin.add(planet.image, function (err) {
-                    if (err) {
-                        console.log(err, planet.image);
-                    }
-                    console.log("Pinnned planet image ", planet.image);
-                });
-            }
-            if (pinset) {
-                console.log('Planet image already pinned', pinset, planet.image);
-            }
+            ipfs.add(metadataBuffer, { pin: true }, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return false;
+                }
+                console.log("planet ", assetName, " hash ", result[0].hash);
+                data.metadata = result[0].hash;
+                // console.log(data);
+                assetData.push(data);
+                if (i === gifFiles.length -1){
+                    console.log(assetData); 
+                    const jsonData = JSON.stringify(assetData); 
+                    fs.writeFile(outputFile, jsonData, function(err) {
+                        if(err) {
+                            return console.log(err);
+                            process.exit(1);
+                        }
+                        console.log("The file was saved!");
+                    }); 
+                }
+            })
         })
-    }
+
+    }  // close for loop
 
 
-    galaxiaInstance.events.UpgradePathAdded({
-        filter: {},
-        fromBlock: 0
-    }, async (error, event) => {
-        if (error) console.log(error);
-        if (event) {
-            const fullHash = event.returnValues._newURI;
-            console.log("upgrade path added: full hash ", fullHash); 
-            if (typeof fullHash !== 'undefined' || fullHash !== "") {
-                const hash = fullHash.substring(5); // returns short path ipfs/Qm....
-                console.log("Upgrade Path Added: pinning hash ", hash);
-                ipfs.pin.ls(hash, (err, pinset) => {
-                    if (err) {
-                        console.log(err);
-                        ipfs.pin.add(hash, function (err) {
-                            if (err){
-                                console.log(err, hash);
-                            }
-                            console.log("pinned uri upgrade ", hash);
-                        });
-                    }
-                    if (pinset) {
-                        console.log('Hash already pinned', hash);
-                    }
 
-                })
-            }
-        }
-    });
 
-    galaxiaInstance.events.MetadataUpgraded({
-        filter: {},
-        fromBlock: 0
-    }, async (error, event) => {
-        if (error) console.log(error);
-        if (event) {
-            const fullHash = event.returnValues._newURI;
-            if (typeof fullHash !== 'undefined' || fullHash !== "") {
-                const hash = fullHash.substring(5);  // returns short path ipfs/Qm....
-                console.log("Metadata upgraded: pinning hash ", hash);
-                ipfs.pin.ls(hash, (err, pinset) => {
-                    if (err) {
-                        console.log('Pinning hash...', hash);
-                        ipfs.pin.add(hash, function (err) {
-                            if (err){
-                                console.log(err, hash);
-                            }
-                            console.log("pinned upgraded uri ", hash);
-                        });
-                    }
-                    if (pinset) {
-                        console.log('Hash already pinned', hash);
-                    }
-                })
-            }
-        }
-    });
+    // galaxiaInstance.events.UpgradePathAdded({
+    //     filter: {},
+    //     fromBlock: 0
+    // }, async (error, event) => {
+    //     if (error) console.log(error);
+    //     if (event) {
+    //         const fullHash = event.returnValues._newURI;
+    //         console.log("upgrade path added: full hash ", fullHash);
+    //         if (typeof fullHash !== 'undefined' || fullHash !== "") {
+    //             const hash = fullHash.substring(5); // returns short path ipfs/Qm....
+    //             console.log("Upgrade Path Added: pinning hash ", hash);
+    //             ipfs.pin.ls(hash, (err, pinset) => {
+    //                 if (err) {
+    //                     console.log(err);
+    //                     ipfs.pin.add(hash, function (err) {
+    //                         if (err) {
+    //                             console.log(err, hash);
+    //                         }
+    //                         console.log("pinned uri upgrade ", hash);
+    //                     });
+    //                 }
+    //                 if (pinset) {
+    //                     console.log('Hash already pinned', hash);
+    //                 }
 
-    galaxiaInstance.getPastEvents('Transfer', {
-        filter: {},
-        fromBlock: 0,
-        toBlock: 'latest'
-    }, (error, events) => {
-        if (error) throw error;
-        events.forEach(async (event) => {
-            // console.log("EVENT VALUES ", even    t.returnValues);
-            const id = event.returnValues.tokenId;
-            const fullHash = await galaxiaInstance.methods.tokenURI(id).call();
-            // const imageHash = 
-            if (typeof fullHash !== 'undefined' || fullHash !== "") {
-                const hash = fullHash.substring(21);
-                ipfs.pin.ls(hash, (err, pinset) => {
-                    if (err) {
-                        ipfs.pin.add(hash, function (err) {
-                            if (err){ 
-                            console.log(err, hash);
-                            }
-                            console.log("Successfullly pinned hash transfer() ", hash);
-                        });
-                    }
-                    if (pinset) {
-                        console.log('Hash already pinned', pinset, hash);
-                    }
-                });
-            }
+    //             })
+    //         }
+    //     }
+    // });
 
-        });
-    });
-
-    galaxiaInstance.events.Transfer({
-        filter: {},
-        fromBlock: 0
-    }, async (error, event) => {
-        if (error) console.log(error);
-        if (event) {
-            const id = event.returnValues.tokenId;
-            const fullHash = await galaxiaInstance.methods.tokenURI(id).call();
-            if (typeof fullHash !== 'undefined' || fullHash !== "") {
-                const hash = fullHash.substring(21);
-                console.log("Transfer() pinning hash ", hash);
-                ipfs.pin.ls(hash, (err, pinset) => {
-                    if (err) {
-                        console.log('Pinning hash...', hash);
-                        ipfs.pin.add(hash, function (err) {
-                            if (err){
-                                console.log(err, hash);
-                            }
-                            console.log("Pinned hash from transfer() ", hash);
-                        });
-                    }
-                    if (pinset) {
-                        console.log('Hash already pinned', pinset, hash);
-                    }
-                })
-            }
-        }
-    });
+    // galaxiaInstance.events.MetadataUpgraded({
+    //     filter: {},
+    //     fromBlock: 0
+    // }, async (error, event) => {
+    //     if (error) console.log(error);
+    //     if (event) {
+    //         const fullHash = event.returnValues._newURI;
+    //         if (typeof fullHash !== 'undefined' || fullHash !== "") {
+    //             const hash = fullHash.substring(5);  // returns short path ipfs/Qm....
+    //             console.log("Metadata upgraded: pinning hash ", hash);
+    //             ipfs.pin.ls(hash, (err, pinset) => {
+    //                 if (err) {
+    //                     console.log('Pinning hash...', hash);
+    //                     ipfs.pin.add(hash, function (err) {
+    //                         if (err) {
+    //                             console.log(err, hash);
+    //                         }
+    //                         console.log("pinned upgraded uri ", hash);
+    //                     });
+    //                 }
+    //                 if (pinset) {
+    //                     console.log('Hash already pinned', hash);
+    //                 }
+    //             })
+    //         }
+    //     }
+    // });
 
 });
 
