c34dbb3c9855316c22f17c8b596ba9c71d66077a
kyledewy <dewhurst.kyle@protonmail.ch>
2019-08-06 18:31:28 +0200

    promisified callbacks

--- index.js
+++ index.js
@@ -1,110 +1,86 @@
 require('dotenv').config()
 const fs = require('fs');
 const IPFS = require('ipfs');
-const IMAGES_DIR = "./assets/images/";
-const METADATA_DIR = "./assets/metadata/";
+// TODO: Organize deployments better than this
+const IMAGES_DIR = "./assets/images/v1/";
+const METADATA_DIR = "./assets/metadata/v1/";
 const outputFile = "./assets/deployed-assets.json";
+const util = require('util');
 
 
-// Initiate IPFS and Web3
+// Initiate IPFS 
 const ipfs = new IPFS();
-
-
-// ------------------Upload Images------------------------------
-const GifDir = IMAGES_DIR + 'v1';
-const MetaDir = METADATA_DIR + 'v1';
-let gifFiles = [];
-let metadataFiles = [];
-
+// All asset data, used to output json file of ipfs hashes + asset names
 let assetData = [];
 
-// Get planet image files
-fs.readdir(GifDir, function (err, files) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    gifFiles = files;
-    // console.log(files);
-})
 
-// Get planet metadata files
-fs.readdir(MetaDir, function (err, files) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    metadataFiles = files;
-    // console.log(files);
-})
+const readDir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile)
 
-if (metadataFiles.length !== gifFiles.length) {
-    console.log("There are ", metadataFiles.length, " metadata files ", " and ", gifFiles.length, " image files");
-    process.exit(1);
+async function getFileData(data) {
+    return await readFile(data)
 }
 
-ipfs.once('ready', () => {
+async function getFileDir(dir) {
+    return await readDir(dir);
+}
 
-    console.log("IPFS node is ready ");
-    // loop through planets
-    for (let i = 0; i < gifFiles.length; i++) {
-        const GIF = gifFiles[i];
-        const Metadata = metadataFiles[i];
-        console.log(GIF)
-        const assetName = GIF.slice(0, GIF.length - 4);
-        const data = { id: "", imageHash: "", metadata: "" };
-        const gifLocation = `${GifDir}/${GIF}`;
-        const metadataLocation = `${METADATA_DIR}/${Metadata}`;
-        console.log("planet name ", assetName);
-        data.id = i;
-        data.name = assetName;
-
-        fs.readFile(gifLocation, (err, data) => {
-         if (err) throw err;
-         console.log(data);
-         const imageBuffer = Buffer.from(data);
-         console.log(imageBuffer)
-         ipfs.add(imageBuffer, { pin: true }, (err, result) => {
-           if (err) {
-               console.log(err);
-               return false;
-           }
-
-           // console.log("planet ", assetName, " hash ", result[0].hash);
-           console.log("image buffer ", result);
-           data.imageHash = result[0].hash;
+function checkParity(image_files, metadata_files) {
+    if (metadata_files.length !== image_files.length) {
+        console.log("Number of metadata files dont match number of image files!");
+        process.exit(1);
+    }
+}
 
 
-           fs.readFile(gifLocation, (err, data) => {
-            if (err) throw err;
-            console.log(data);
-            const metadataBuffer = Buffer.from(data);
-            console.log(metadataBuffer)
 
-            ipfs.add(metadataBuffer, { pin: true }, (err, result) => {
-              if (err) {
-                  console.log(err);
-                  return false;
-              }
-              // console.log("planet ", assetName, " hash ", result[0].hash);
-              console.log("metadata buffer ", result);
-              data.metadata = result[0].hash;
-              // console.log(data);
-              assetData.push(data);
-              if (i === gifFiles.length -1){
-                  console.log(assetData);
-                  const jsonData = JSON.stringify(assetData);
-                  // fs.writeFile(outputFile, jsonData, function(err) {
-                  //     if(err) {
-                  //          console.log(err);
-                  //         process.exit(1);
-                  //     }
-                  //     console.log("The file was saved!");
-                  // });
-              }
-            })
-          })
-        })
-      })
-    }  // close for loop
+ipfs.once('ready', async () => {
+    try {
+        const gifFiles = await getFileDir(IMAGES_DIR);
+        const metadataFiles = await getFileDir(METADATA_DIR);
+        checkParity(gifFiles, metadataFiles); 
+        console.log("gif files ", gifFiles);
+        console.log("metadata files ", metadataFiles);
+        console.log("IPFS node is ready ");
+        // loop through planets
+        for (let i = 1; i <= gifFiles.length; i++) {
+            const GIF = gifFiles[i];
+            const Metadata = metadataFiles[i];
+            const assetName = GIF.slice(0, GIF.length - 4);
+            const data = { id: "", imageHash: "", metadata: "" };
+            const gifLocation = `${IMAGES_DIR}/${GIF}`;
+            const metadataLocation = `${METADATA_DIR}${Metadata}`;
+            data.id = i;
+            data.name = assetName;
+
+            // upload gif image
+            const gif_data = await getFileData(gifLocation);
+            const image_buffer = Buffer.from(gif_data);
+            const ipfs_gif = await ipfs.add(image_buffer, { pin: true });
+            data.imageHash = ipfs_gif[0].hash;
+
+            // Upload metadata
+            const metadata_data = await getFileData(metadataLocation);
+            const metadata_buffer = Buffer.from(metadata_data);
+            const ipfs_metadata = await ipfs.add(metadata_buffer);
+            data.metadata = ipfs_metadata[0].hash;
+            // Add data to list
+            console.log(data.name, "image hash: ", data.imageHash, " metadata hash: ", data.metadata);
+            assetData.push(data);
+        }  // close for loop
+
+        console.log(assetData);
+        const jsonData = JSON.stringify(assetData);
+        fs.writeFile(outputFile, jsonData, function (err) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            console.log("The file was saved!");
+        });
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
 });
